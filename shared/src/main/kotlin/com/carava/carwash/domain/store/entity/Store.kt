adf10d19ed087925771636f82c904e7a1446edd1
yujin0124 <yujin0124@khu.ac.kr>
2025-09-09 22:02:29 +0900

    refactor: Add detailed fields to Store entity

--- shared/src/main/kotlin/com/carava/carwash/domain/store/entity/Store.kt
+++ shared/src/main/kotlin/com/carava/carwash/domain/store/entity/Store.kt
@@ -3,6 +3,7 @@ package com.carava.carwash.domain.store.entity
 import com.carava.carwash.common.entity.BaseEntity
 import com.carava.carwash.common.exception.ForbiddenException
 import jakarta.persistence.*
+import java.math.BigDecimal
 
 @Entity(name = "store")
 @Table(name = "store")
@@ -12,14 +13,40 @@ class Store (
     var id: Long = 0,
 
     @Column(nullable = false)
-    var name: String,
+    var ownerMemberId: Long = 0,
 
     @Column(nullable = false)
-    var ownerMemberId: Long = 0,
+    var name: String,
+
+    @Column(columnDefinition = "TEXT")
+    var description: String? = null,
+
+    @Column(length = 20)
+    var phone: String?= null,
+
+    //TODO: 주소 entity 만들고 연결
+    @Column(nullable = false)
+    var addressId: Long = 0,
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     var category: StoreCategory = StoreCategory.CAR_WASH,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    var status: StoreStatus = StoreStatus.ACTIVE,
+
+    @Column(precision = 2, scale = 1)
+    var averageRating: BigDecimal = BigDecimal("0.0"),
+
+    @Column(columnDefinition = "INT DEFAULT 0")
+    var totalReviews: Int = 0,
+
+    @Column(columnDefinition = "INT DEFAULT 0")
+    var viewCount: Int = 0,
+
+    @Column(columnDefinition = "INT DEFAULT 0")
+    var favoriteCount: Int = 0,
 
 ) : BaseEntity() {
 
